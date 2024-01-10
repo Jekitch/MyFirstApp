5d1e498d447fa4fffe32a1cb1237778dfb4997fa
Jekitch <94888253+Jekitch@users.noreply.github.com>
2024-01-10 04:15:57 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -31,5 +31,50 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        //my code
+        """
+        1. работой с преобразованием строк
+        2. базовой арифметикой и выводом результата в консоль
+        3. примерами интерполяции сток
+        4. преобразованием типов
+        5. примером условных операторов (if else) 
+        6. примером с циклами (for while) 
+        7. примером создания отдельной функции
+        8. примером работы с массивом
+    """
+        //1
+            var data : String; data = "hello world!";
+            data = data.substring(1..7).toUpperCase();
+            println(data)
+        //2
+            var a = 90; var b = 10; println("a+b = ${a+b}; a*b = ${a*b}; a/b = ${a/b}; a-b = ${a-b}; ")
+        //3
+            var content = "world!"; println("hello ${content}");
+        //4
+            var STRINGVAR : String = "1234"; println( (STRINGVAR.toInt() - 1000).toString() );
+        //5
+            var BOOL = false;
+            if (BOOL) { println("1 Is True"); } else { println("1 Is False"); }
+            BOOL = true;
+            if (BOOL) { println("2 Is True"); } else { println("2 Is False"); }
+            //6.1
+            var I : Int = 0;
+            while (I < 10) { print(I); I++; }
+            println("");
+        //6.2
+            for (i in 1..10) { print(i); }
+        //7
+            fun FUCT(x: Int) : String { var res = 1; for (i in 1..x) { res *= i; }; return "\nфакториал ${x} равен ${res}" }
+            println(FUCT(5))
+        //8
+            var array = arrayOf("h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d");
+            var array0 = Array(2, {20});
+            println()
+            array[4] = "HELLO"; array[6] = "WORLD";
+            println(array.size)
+            println(array[4] + " " + array[6])
+            var res = 0;
+            for (i in array0) { res += i; }; println(res);
     }
 }
